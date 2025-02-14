b0c70d0d78df63383f588a26ca6485be8cf14b31
jekabsgais <gaisinsj@gmail.com>
2025-02-14 15:37:08 +0200

    14.02.25

---
+++ II.semestris/14.02/script.js
@@ -0,0 +1,40 @@
+//alert("Hello World!");
+
+
+
+/*
+let x=5;
+let y=10
+alert(x);
+console.log ("x+y = " , x+y);
+console.log("x+y = " ,x-y);
+console.log("x+y = " ,x/y);
+console.log("x+y = " ,x*y);
+console.log("x+y = " ,x**y);
+*/
+
+/*
+let number =prompt("Enter number");
+if(number >0)
+    {
+console.log(number,"ir pozitīvs");
+}
+if(number <0)
+    {
+console.log(number,"ir negatīvs");
+}
+if(number== 0)
+    {
+console.log(number,"ir 0");
+}
+*/
+let age = 18;
+let citizenship =false;
+if(age>=18 && citizenship)
+{
+    console.log("var balsot");
+}
+if(age>=18 && citizenship==false)
+{
+    console.log("nav pilsonis, bet ir 18");
+}
