5bf088862d4a96e0660e33a9dc89eefa611492f0
jekabsgais <gaisinsj@gmail.com>
2025-03-21 15:42:36 +0200

    .

---
+++ II.semestris/21.03/script.js
@@ -0,0 +1,24 @@
+/*
+let array = [];
+console.log(array);
+
+array.push(1);
+array.push(2);
+array.push(3)
+array.push(4);
+console.log(array);
+array.unshift(0);
+
+console.log(array[4]);
+
+array.pop()
+console.log(array);
+array.shift();
+console.log(array);
+*/
+
+let mas1= [1,2,3,4,5,6,7,8,9,10];
+for(let i=0; i<10; i++)
+{
+    console.log(mas1[i]);
+}
